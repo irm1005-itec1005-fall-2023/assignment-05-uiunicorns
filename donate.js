b9dbd0d63ce28c98c0e9c3f57ecad0f7476ebc22
Harry Tromans <harrytromans@hotmail.com>
2023-12-04 18:44:09 -0500

    Added code for button functionality

--- donate.js
+++ donate.js
@@ -15,8 +15,71 @@ let Name = "";
 let Email = "";
 let Phone = "";
 
+let form = document.getElementById("test");
+
+let ten = document.getElementById("btn10");
+let twenty = document.getElementById("btn20");
+let thirty = document.getElementById("btn30");
+let fourty = document.getElementById("btn40");
+
+let field = document.getElementById("cstm");
+let signUp = document.getElementById("myCheck");
+
 
 // Functions
+
+chosen = [0];
+YN = [false];
+
+ten.addEventListener("click", select, false);
+twenty.addEventListener("click", select, false);
+fourty.addEventListener("click", select, false);
+thirty.addEventListener("click", select, false);
+field.addEventListener("click", select,false);
+signUp.addEventListener("click", select,false)
+
+function select () {
+  if (this.id == "btn10") {
+      //css goes here
+      chosen [0] = 1;
+  }
+  else if (this.id == "btn20") {
+      //css goes here
+      chosen [0] = 2;
+  } 
+  else if (this.id == "btn30") {
+      //css goes here
+      chosen [0] = 3;
+  }
+  else if (this.id == "btn40") {
+      //css goes here
+      chosen [0] = 4;
+  }
+  else if (this.id == "cstm") {
+      chosen [0] = 5;
+  }
+  
+  if (this.id == "myCheck" && YN[0] == false) {
+      YN [0] = true;
+  } else {
+      YN [0] = false;
+  }
+  console.log(chosen);
+  console.log(YN);
+}
+
+
+
+form = document.addEventListener("submit", formInput);
+
+function formInput(event) {
+  //console.log("huh");
+  
+}
+
+
+
+
 
 function inititialise() {
 
