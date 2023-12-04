ab7b0eebb7de26cac31357221bf9460377dc4d3e
callumtate <callum.tate@carleton.ca>
2023-12-04 14:47:53 -0500

    Create variables

--- donate.js
+++ donate.js
@@ -1,8 +1,22 @@
 //Js for donate page
 //general code for making the overall donation page work
+
+// Variables
 const h1 = document.createElement("h1");
+const appID = "donate";
+const donateButton = document.createElement("button");
 
 let appContainer = document.getElementById(appID);
+
+
+let donateValue = 0;
+let rec = false;
+let Name = "";
+let Email = "";
+let Phone = "";
+
+
+// Functions
 
 function inititialise() {
 
