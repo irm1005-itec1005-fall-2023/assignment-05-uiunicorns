05830302b0d07e6f686c8cc8b44c117eb4c2cb22
callumtate <callum.tate@carleton.ca>
2023-12-04 14:39:12 -0500

    create js file for donate page

---
+++ donate.js
@@ -0,0 +1,17 @@
+//Js for donate page
+//general code for making the overall donation page work
+const h1 = document.createElement("h1");
+
+let appContainer = document.getElementById(appID);
+
+function inititialise() {
+
+h1.textContent = "Donate";
+appContainer.appendChild(h1);
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Your code here
+});
+}
+
+inititialise();
